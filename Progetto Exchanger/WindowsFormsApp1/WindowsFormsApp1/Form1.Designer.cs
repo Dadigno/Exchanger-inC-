a32dd8663fc34d90f459f4e4e02612fb5ef10a69
Davide <daviderng@gmail.com>
2018-02-17 21:55:11 +0100

    repository risistemata

---
+++ Progetto Exchanger/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace BasicChangeCurrencies
+{
+    partial class Form1
+    {
+        public object[] FiatCoins = new object[2] { "USD", "EUR"};
+        public object[] CryptoCoins = new object[3] { "Bitcoin", "Ethereum", "Litecoin" };
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.compute = new System.Windows.Forms.Button();
+            this.txtFrom = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.percentage = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.Label();
+            this.amount = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.rate = new System.Windows.Forms.Label();
+            this.reloadCur = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonSetToday = new System.Windows.Forms.Button();
+            this.comboFromCur = new System.Windows.Forms.ComboBox();
+            this.comboToCur = new System.Windows.Forms.ComboBox();
+            this.selectIco = new System.Windows.Forms.RadioButton();
+            this.selectFiat = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // compute
+            // 
+            resources.ApplyResources(this.compute, "compute");
+            this.compute.BackColor = System.Drawing.SystemColors.Desktop;
+            this.compute.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.compute.ForeColor = System.Drawing.Color.Lime;
+            this.compute.Name = "compute";
+            this.compute.UseVisualStyleBackColor = false;
+            this.compute.Click += new System.EventHandler(this.ButtonCalc);
+            // 
+            // txtFrom
+            // 
+            resources.ApplyResources(this.txtFrom, "txtFrom");
+            this.txtFrom.Name = "txtFrom";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // percentage
+            // 
+            resources.ApplyResources(this.percentage, "percentage");
+            this.percentage.ForeColor = System.Drawing.Color.Lime;
+            this.percentage.Name = "percentage";
+            // 
+            // txtAmount
+            // 
+            resources.ApplyResources(this.txtAmount, "txtAmount");
+            this.txtAmount.Name = "txtAmount";
+            // 
+            // amount
+            // 
+            resources.ApplyResources(this.amount, "amount");
+            this.amount.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.amount.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.amount.Name = "amount";
+            // 
+            // result
+            // 
+            resources.ApplyResources(this.result, "result");
+            this.result.ForeColor = System.Drawing.Color.Yellow;
+            this.result.Name = "result";
+            // 
+            // txtResult
+            // 
+            resources.ApplyResources(this.txtResult, "txtResult");
+            this.txtResult.Name = "txtResult";
+            // 
+            // rate
+            // 
+            resources.ApplyResources(this.rate, "rate");
+            this.rate.BackColor = System.Drawing.Color.Transparent;
+            this.rate.Name = "rate";
+            // 
+            // reloadCur
+            // 
+            resources.ApplyResources(this.reloadCur, "reloadCur");
+            this.reloadCur.BackColor = System.Drawing.Color.SteelBlue;
+            this.reloadCur.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.reloadCur.Name = "reloadCur";
+            this.reloadCur.UseVisualStyleBackColor = false;
+            this.reloadCur.Click += new System.EventHandler(this.ButtonReload);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.reloadCur);
+            this.groupBox1.Controls.Add(this.rate);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.buttonSetToday);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
+            // 
+            // buttonSetToday
+            // 
+            resources.ApplyResources(this.buttonSetToday, "buttonSetToday");
+            this.buttonSetToday.BackColor = System.Drawing.Color.SteelBlue;
+            this.buttonSetToday.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSetToday.Name = "buttonSetToday";
+            this.buttonSetToday.UseVisualStyleBackColor = false;
+            this.buttonSetToday.Click += new System.EventHandler(this.ButtonSetToday_Click);
+            // 
+            // comboFromCur
+            // 
+            resources.ApplyResources(this.comboFromCur, "comboFromCur");
+            this.comboFromCur.FormattingEnabled = true;
+            this.comboFromCur.Name = "comboFromCur";
+            
+            // 
+            // comboToCur
+            // 
+            resources.ApplyResources(this.comboToCur, "comboToCur");
+            this.comboToCur.FormattingEnabled = true;
+            this.comboToCur.Name = "comboToCur";
+            // 
+            // selectIco
+            // 
+            resources.ApplyResources(this.selectIco, "selectIco");
+            this.selectIco.Checked = true;
+            this.selectIco.Name = "selectIco";
+            this.selectIco.TabStop = true;
+            this.selectIco.UseVisualStyleBackColor = true;
+            this.selectIco.CheckedChanged += new System.EventHandler(this.selectIco_CheckedChanged);
+            // 
+            // selectFiat
+            // 
+            resources.ApplyResources(this.selectFiat, "selectFiat");
+            this.selectFiat.Name = "selectFiat";
+            this.selectFiat.TabStop = true;
+            this.selectFiat.UseVisualStyleBackColor = true;
+            this.selectFiat.CheckedChanged += new System.EventHandler(this.selectFiat_CheckedChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Controls.Add(this.selectFiat);
+            this.Controls.Add(this.selectIco);
+            this.Controls.Add(this.comboToCur);
+            this.Controls.Add(this.comboFromCur);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.percentage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.compute);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        
+
+        #endregion
+
+        private System.Windows.Forms.Button compute;
+        private System.Windows.Forms.Label txtFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label percentage;
+        private System.Windows.Forms.Label txtAmount;
+        private System.Windows.Forms.TextBox amount;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Label rate;
+        private System.Windows.Forms.Button reloadCur;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSetToday;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboToCur;
+        private System.Windows.Forms.RadioButton selectIco;
+        private System.Windows.Forms.RadioButton selectFiat;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboFromCur;
+    }
+}
+
---
+++ Progetto Exchanger/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+
+namespace BasicChangeCurrencies
+{
+    public partial class Form1 : Form
+    {
+        
+        public DateTime dataInput = DateTime.Now;
+        
+        public Form1()
+        {
+            Text = "urrencies Exchange";
+            InitializeComponent();
+            comboFromCur.Items.AddRange(CryptoCoins);
+            comboToCur.Items.AddRange(FiatCoins);
+        }
+
+        private void ButtonCalc(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectFiat.Checked)
+                {
+                    rate.Text = WebServer.GetConversionRate(comboFromCur.Text, comboToCur.Text, dataInput);
+                    result.Text = WebServer.GetConversionAmount(comboFromCur.Text, comboToCur.Text, dataInput, amount.Text.Replace(",", "."));
+                }
+                else
+                {
+                    if (selectIco.Checked)
+                    {
+                        rate.Text = WebServer.GetConversionRate(comboFromCur.Text, comboToCur.Text);
+                        result.Text = WebServer.CalculateIcoChange(Convert.ToDouble(amount.Text.Replace(".", ",")), rate.Text);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error invalid input", "Convert currencies", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void ButtonReload(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboFromCur.Text != null && comboToCur.Text != null)
+                {
+                    if (selectFiat.Checked == true)
+                    {
+                        rate.Text = WebServer.GetConversionRate(comboFromCur.Text, comboToCur.Text, dataInput);   // OBTAIN FIAT CHANGE 
+                    }
+                    else
+                    {
+                        rate.Text = WebServer.GetConversionRate(comboFromCur.Text, comboToCur.Text);       // OBTAIN ICO CHANGE  (USD to ICO)
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Error currencies not valid", "Convert currencies", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (System.Net.WebException)
+            {
+                rate.Text = "Error!! Currencies not valid or there is no connection";
+            }
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dateTimePicker1.Value > DateTime.Now) 
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    dataInput = dateTimePicker1.Value;
+                }
+            }
+            catch (ArgumentException)
+            {
+                dataInput = DateTime.Now;
+                MessageBox.Show("You cant go in the future!!");
+            }
+        }
+
+        private void ButtonSetToday_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today;
+        }
+
+        private void selectIco_CheckedChanged(object sender, EventArgs e)
+        {
+            comboToCur.Text = "EUR";
+            comboFromCur.Text = "bitcoin";
+            groupBox2.Enabled = false;
+            comboFromCur.Items.Clear();
+            comboFromCur.Items.AddRange(CryptoCoins);
+        }
+
+        private void selectFiat_CheckedChanged(object sender, EventArgs e)
+        {
+            comboToCur.Enabled = true;
+            groupBox2.Enabled = true;
+            comboFromCur.Text = "USD";
+            comboToCur.Items.Clear();
+            comboToCur.Items.AddRange(FiatCoins);
+            comboFromCur.Items.Clear();
+            comboFromCur.Items.AddRange(FiatCoins);
+        }
+        
+    }
+
+    public class WebServer
+    {
+        public static WebClient WebExchange = new WebClient();
+        public static string format = "MM/dd/yyyy";
+
+        /// FROM CURRENCY CONVERTER.KOWABUNG.NET
+        public static string GetConversionRate(string fCur, string toCur, DateTime data)
+        {
+            string urlPattern = "http://currencyconverter.kowabunga.net/converter.asmx/GetConversionRate?CurrencyFrom={0}&CurrencyTo={1}&RateDate={2}";
+            string date = data.ToString(format);
+            string response = Regex.Replace(WebExchange.DownloadString(string.Format(urlPattern, fCur, toCur, date)), "<.*?>", String.Empty).Remove(0, 2);
+            return response;
+        }
+        public static string GetConversionAmount(string fCur, string toCur, DateTime data, string amount)
+        {
+            string urlPattern = "http://currencyconverter.kowabunga.net/converter.asmx/GetConversionAmount?CurrencyFrom={0}&CurrencyTo={1}&RateDate={2}&Amount={3}";
+            string date = data.ToString(format);
+            string response = Regex.Replace(WebExchange.DownloadString(string.Format(urlPattern, fCur, toCur, date, amount)), "<.*?>", String.Empty).Remove(0, 2).Replace('.', ',');
+            return response;
+        }
+
+        //FROM COINMARKET.COM
+        public static string GetConversionRate(string FromCur, string ToCur)
+        {
+            string response;
+            char[] arCharSep = new char[] { ' ', ',' };
+            string urlPattern = "https://api.coinmarketcap.com/v1/ticker/{0}/?convert=EUR";
+            string text = WebExchange.DownloadString(string.Format(urlPattern, FromCur, ToCur));
+            string[] segment = text.Split(arCharSep, StringSplitOptions.RemoveEmptyEntries);
+            if(ToCur== "EUR")
+            response = segment[48].Replace("\"", "").Replace(".", ",");
+            else
+                response = segment[15].Replace("\"", "").Replace(".", ",");
+            return response;
+        }
+        //public static double GetConversioRate(string toCur, int amount)
+        public static string CalculateIcoChange(double amount, string rate)
+        {
+            string s = Convert.ToString(amount * Convert.ToDouble(rate.Replace(".", ",")));
+            return string.Concat(s, "€");
+        }
+
+    }
+}
